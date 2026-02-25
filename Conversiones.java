df02a60cfed151f9cff6f350c4053d2b5cd1ace3
Wisi <wisi@a.com>
2026-02-25 18:57:42 +0100

    menutilidades FarenheitACelsius

--- Conversiones.java
+++ Conversiones.java
@@ -1,11 +1,21 @@
 public class Conversiones {
 
     public static double celsiusAFahrenheit(double c) {
+    	
         return (c * 9.0 / 5.0) + 32.0;
     }
+    
+    public static double fahrenheitACelsius(double f) {
+    	
+        return (f-32)*5/9;
+    }
 
-    public static double kmAMillas(double km) {
+     public static double kmAMillas(double km) {
         return km * 0.621371;
+    }
+    
+    public static double millasKm(double mll) {
+        return mll * 1.60934;
     }
 
     public static double metrosACm(double m) {
--- MenuUtilidades.java
+++ MenuUtilidades.java
@@ -12,6 +12,9 @@ public class MenuUtilidades {
             System.out.println("3) ¿Es palíndromo?");
             System.out.println("4) Celsius a Fahrenheit");
             System.out.println("5) Segundos desde fecha (AAAA-MM-DD HH:MM)");
+            System.out.println("6) Kilómetros a millas");
+            System.out.println("7) Kilómetros a millas a la inversa");
+            System.out.println("8) Fahrenheit a Celsius");
             System.out.println("0) Volver");
 
             opcion = Entrada.leerEntero(sc, "Opción: ");
@@ -26,14 +29,24 @@ public class MenuUtilidades {
                 String t = Entrada.leerTexto(sc, "Texto: ");
                 System.out.println("Palíndromo = " + Cadenas.esPalindromo(t));
             } else if (opcion == 4) {
-                double c = Entrada.leerDouble(sc, "Celsius: ");
+                double  c = Entrada.leerDouble(sc, "Celsius: ");
                 System.out.println("F = " + Conversiones.celsiusAFahrenheit(c));
             } else if (opcion == 5) {
                 LocalDateTime dt = Entrada.leerFechaHoraSimple(sc, "Fecha y hora (AAAA-MM-DD HH:MM): ");
                 long s = Fechas.segundosDesde(dt);
                 System.out.println("Segundos = " + s);
                 System.out.println("Formateado = " + Fechas.formatearSegundos(s));
-            } else if (opcion != 0) {
+            } else   if (opcion == 6) {
+                double km = Entrada.leerDouble(sc, "Kilómetros: ");
+                System.out.println("Millas = " + Conversiones.kmAMillas(km));
+            } else   if (opcion == 7) {
+                double km = Entrada.leerDouble(sc, "Millas: ");
+                System.out.println("Kilometros = " + Conversiones.millasKm(km));
+            }else if (opcion == 8) {
+                double  c = Entrada.leerDouble(sc, "Farenheit: ");
+                System.out.println("C = " + Conversiones.fahrenheitACelsius(c));
+            }
+            else if (opcion != 0) {
                 System.out.println("Opción no válida.");
             }
 
